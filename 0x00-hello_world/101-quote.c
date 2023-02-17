f3f5d88c9008b25f68f02247ca5404bd06fa5b23
edikanomoijuanfo <edhykhan18@gmail.com>
2023-02-16 17:25:53 -0800

    advanced C programming task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Standard error
+ * Return: Always 1
+ * /
+ int main(void)
+ {
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return(1);
+}
