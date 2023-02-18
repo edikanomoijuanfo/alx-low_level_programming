96beaca2837eb16e2ee8e35e212b8c7303f70467
edikanomoijuanfo <edhykhan18@gmail.com>
2023-02-18 09:46:29 -0800

    if,else,while task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints alphabets in lower case
+ * main - Entry
  * Return: Always 0
  */
 int main(void)
 {	char lc;
 	for (lc = 'a'; lc <= 'z'; lc++)
-		;
-	{	putchar(lc);
+	{
+		putchar(lc);
 	}
 	putchar('\n');
 	return (0);
