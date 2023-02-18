57eb1c7be81e3c58f54ccf0ade3005062d8e2f7e
edikanomoijuanfo <edhykhan18@gmail.com>
2023-02-18 10:00:14 -0800

    if,else,while task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,19 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		if (n != 57)
+		if (n == '9')
+		{
+			break;
+		}
+		else
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
-	}
 }
