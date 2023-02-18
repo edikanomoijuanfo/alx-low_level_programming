c22501f590bb8c211d5476df7696edd6f34714f9
edikanomoijuanfo <edhykhan18@gmail.com>
2023-02-18 02:58:00 -0800

    code corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Standard error
+ * main - Entry
  * Return: Always 1
- * /
- int main(void)
- {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
